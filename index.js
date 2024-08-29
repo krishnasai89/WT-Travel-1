d21e1684ba4b273cf26ce497af6c25bf8dad3830
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-08-29 21:59:20 +0530

    Merge remote changes

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function toggleReadMore() {
 document.getElementById('currency').addEventListener('change', function () {
     alert('Selected currency: ' + this.value);
 });
+
+
+
+
+
+// Get the button:
+let mybutton = document.getElementById("myButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
