49ec501b8e75bd8d3a10c65dc6dbdad6ad5f7a2b
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-09-27 11:38:08 +0530

    updated

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ let topFunction = () => {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// read more
+function myFunction() {
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
