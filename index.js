d978bab0fe9c7f3acfdb17859e69c929c9c16b9e
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-10-02 17:36:13 +0530

    upadate index.html

--- index.js
+++ index.js
@@ -34,3 +34,14 @@ function myFunction() {
         moreText.style.display = "inline";
     }
 }
+
+// loader
+var myVar;
+
+loader=()=>{
+    myVar = setTimeout(showPage, 3000);
+}
+showPage=()=>{
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
